d8d8ee016de1767772106221b2771683fd701ec1
Ethlyo <77252686+Ethlyo@users.noreply.github.com>
2021-05-31 13:01:12 -0700

    First Commit (Attempted Home, Footer, Header)

---
+++ app/Controllers/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+	public function index()
+	{
+		$this->load->view("header");
+        $this->load->view("my_welcome_message");
+        $this->load->view("footer");
+	}
+}
