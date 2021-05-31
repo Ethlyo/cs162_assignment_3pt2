f714d55d72547aa2b6ef39a1adfc839c8487a89a
Ethlyo <77252686+Ethlyo@users.noreply.github.com>
2021-05-31 13:18:23 -0700

    Attempt 3 loading views

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,10 @@ class Home extends BaseController
 {
 	public function index()
 	{
+		$data = [
+			'page_title' => 'Your title'
+		];
+		
 		$this->load->view('header');
         $this->load->view('my_welcome_message');
         $this->load->view('footer');
